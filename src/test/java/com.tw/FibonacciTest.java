fced65cf097e0b409d158d3603aa6cbe31a0b078
puwang <puwang@thoughtworks.com>
2017-07-12 00:08:42 +0800

    support when n is 1

--- src/test/java/com.tw/FibonacciTest.java
+++ src/test/java/com.tw/FibonacciTest.java
@@ -10,6 +10,11 @@ public class FibonacciTest {
         assertEquals(0, fibonacci(0));
     }
 
+    @Test
+    public void test_when_n_is_1() throws Exception {
+        assertEquals(1, fibonacci(1));
+    }
+
     private int fibonacci(int n) {
         return n;
     }
