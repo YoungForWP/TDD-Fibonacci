658fd37297431a0fcd8de9fdd5a0d5a8fcfe657a
puwang <puwang@thoughtworks.com>
2017-07-12 00:12:13 +0800

    support when n bigger than 2

--- src/test/java/com.tw/FibonacciTest.java
+++ src/test/java/com.tw/FibonacciTest.java
@@ -15,7 +15,20 @@ public class FibonacciTest {
         assertEquals(1, fibonacci(1));
     }
 
+    @Test
+    public void test_when_n_bigger_than_2() throws Exception {
+        assertEquals(1, fibonacci(2));
+        assertEquals(2, fibonacci(3));
+        assertEquals(3, fibonacci(4));
+        assertEquals(5, fibonacci(5));
+        assertEquals(8, fibonacci(6));
+        assertEquals(13, fibonacci(7));
+    }
+
     private int fibonacci(int n) {
+        if( n >= 2){
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
         return n;
     }
 }
